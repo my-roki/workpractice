90750cfab8decaf70628809b6e5bf4ba8d1c503c
cryoon <anthonychangrok@gmail.com>
2021-03-08 18:48:55 +0900

    auto_task commit. rename test.py to auto_task.py RPA_Basic practice. complete 6_image_recognition.py

--- RPA_basic/2_Desktop/5_screen.py
+++ RPA_basic/2_Desktop/5_screen.py
@@ -1,5 +1,17 @@
 import pyautogui
 
-# 스크린샷 찍기
-img = pyautogui.screenshot()
-img.save("screenshot.png")  # 파일로 저장
+# # 스크린샷 찍기
+# img = pyautogui.screenshot()
+# img.save("screenshot.png")  # 파일로 저장
+
+# pyautogui.mouseInfo()
+# 99,130 255,0,0 #FF0000
+
+pixel = pyautogui.pixel(99, 130)
+print(pixel)
+
+
+print(pyautogui.pixelMatchesColor(99, 130, (255, 0, 0)))
+print(pyautogui.pixelMatchesColor(99, 130, pixel))
+print(pyautogui.pixelMatchesColor(99, 130, (255, 255, 0)))
+
---
+++ RPA_basic/2_Desktop/6_image_recognition.py
@@ -0,0 +1,87 @@
+import pyautogui
+
+# file_menu = pyautogui.locateOnScreen("file_menu.png")
+# print(file_menu)
+# pyautogui.click(file_menu)
+
+# x_icon = pyautogui.locateOnScreen("X_icon.png")
+# pyautogui.moveTo(x_icon)
+
+# screen = pyautogui.locateOnScreen("screenshot.png")
+# print(screen)
+
+# for i in pyautogui.locateAllOnScreen("checkbox.png"):   # 여려개의 이미지를 조정하고 싶을 때
+#     print(i)
+#     pyautogui.click(i, duration=0.25)
+
+# checkbox = pyautogui.locateOnScreen("checkbox.png")  # 제일 처음 만난 이미지에 해당하는 그림을 실행
+# pyautogui.click(checkbox)
+
+# 속도 개선
+# 1. GrayScale : 흑백모드
+# checkbox = pyautogui.locateOnScreen("checkbox.png", grayscale=True)
+# pyautogui.click(checkbox)
+
+# 2. 범위를 지정
+# checkbox = pyautogui.locateOnScreen("checkbox.png", region = (x, y, width, height)
+# pyautogui.click(checkbox)
+
+# 3. 정확도 조정
+# pip install opencv-python
+# run_btn = pyautogui.locateOnScreen("run.png", confidence=0.9)
+# pyautogui.moveTo(run_btn)
+
+# 자동화 대상 이미지가 바로 보여지지 않는 경우
+file_notepad = pyautogui.locateOnScreen("file_notepad.png")
+
+# # 1. 계속 기다리기
+# if file_notepad:
+#     pyautogui.click(file_notepad)
+# else:
+#     print("cannot find location")
+
+# while file_notepad is None:
+#     file_notepad = pyautogui.locateOnScreen("file_notepad.png")
+#     print("cannot find location")
+# pyautogui.click(file_notepad)
+
+# 2. 일정시간 기다리기
+import time
+import sys
+
+
+# timeout = 5  # 대기시간
+# start_time = time.time()  # 시작시간 설정
+# file_notepad = None
+# while file_notepad is None:
+#     file_notepad = pyautogui.locateOnScreen("file_notepad.png")
+#     end_time = time.time()  # 종료시간 설정
+#     if end_time - start_time > timeout:  # 지정한 시간을 초과하면 종료
+#         print("time over")
+#         sys.exit()
+#
+# pyautogui.click(file_notepad)
+
+def find_target(img_file, timeout=10):
+    start_time = time.time()  # 시작시간 설정
+    target = None
+    while target is None:
+        target = pyautogui.locateOnScreen(img_file)
+        end_time = time.time()  # 종료시간 설정
+        if end_time - start_time > timeout:  # 지정한 시간을 초과하면 종료
+            break
+    return target
+
+
+def my_click(img_file, timeout=10):
+    target = find_target(img_file, timeout)
+    if target:
+        pyautogui.click(target)
+        print("Target found")
+    else:
+        print("[Time out {}s] Target not found \"{}\". Terminate program.".format(timeout, img_file))
+        sys.exit()
+
+
+my_click("file_notepad.png", 5)
+
